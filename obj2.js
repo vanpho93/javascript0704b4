a9cdfbd360637ea0bcb9032a41e03b2ba29778cd
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-14 20:49:50 +0700

    xong

--- obj2.js
+++ obj2.js
@@ -13,8 +13,23 @@ function Line(diemA, diemB) {
     }
 }
 
+function Triangle(p1, p2, p3) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.p3 = p3;
+    this.getChuVi = function() {
+        const l1 = new Line(this.p1, this.p2);
+        const l2 = new Line(this.p1, this.p3);
+        const l3 = new Line(this.p3, this.p2);
+        return l1.getLength() + l2.getLength() + l3.getLength();
+    }
+}
+
 const A = new Point(0, 1);
 const B = new Point(1, 0);
+const C = new Point(0, 0);
 
 const AB = new Line(A, B);
-console.log(AB.getLength());
+const ABC = new Triangle(A, B, C);
+// console.log(AB.getLength());
+console.log(ABC.getChuVi());
