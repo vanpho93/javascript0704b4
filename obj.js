07726167b6271c6b96294b94a74ed915792e2818
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-14 20:17:56 +0700

    xong

--- obj.js
+++ obj.js
@@ -1,25 +1,34 @@
 const khoa = { name: 'Khoa Pham', age: 30 };
 const huong = { name: 'Huong', age: 29 };
 
-function Nguoi(ten, tuoi) {
+function Nguoi(ten, tuoi, nguoi) {
     this.name = ten;
     this.age = tuoi;
+    this.wife = nguoi;
     this.show = function() {
         console.log(this.name + '::' + this.age);
     }
+    this.getAge = function() {
+        return this.age;
+    }
+    this.growUp = function() {
+        this.age++;
+    }
 }
 
-const phat = new Nguoi('Phat', 18);
+const phat = new Nguoi('Phat', 18, huong);
+phat.growUp();
+console.log(JSON.stringify(phat));
 
-phat.show();
+// phat.show();
 
-console.log(phat.age);
-// console.log(khoa.name);
-// console.log(huong);
+// console.log(phat.age);
+// // console.log(khoa.name);
+// // console.log(huong);
 
-function Point(x, y) {
-    this.x = x;
-    this.y = y;
-}
+// function Point(x, y) {
+//     this.x = x;
+//     this.y = y;
+// }
 
-const diemO = new Point(0, 0);
+// const diemO = new Point(0, 0);
